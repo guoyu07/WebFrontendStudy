a869eb6eb170a0a31e1e8a11e1ba6b1eef67e82c
siberiawolf <yuxiangliang0307@gmail.com>
2017-02-26 19:16:50 +0800

    整理文件

--- vueJS/first-vue/src/store.js
+++
@@ -1,8 +0,0 @@
-export default{
-	fetch (){
-		return JSON.parse(window.localStorage.getItem('vue-todo'))||[];
-	},
-	save (items) {
-		window.localStorage.setItem('vue-todo',JSON.stringify(items));
-	}
-}
